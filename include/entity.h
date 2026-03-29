3871e54f9168e501fde1e0cd18b9ea433f06fcb2
Willard Nilges <wdn5796@rit.edu>
2026-03-28 22:27:55 -0400

    checkpoint: add dynamic arrays code via copypasta

--- include/entity.h
+++ include/entity.h
@@ -6,3 +6,20 @@
 // transform
 
 // a level/scene could have lighting information
+
+#ifndef ENTITY_H
+#define ENTITY_H
+//#include "cglm/types.h"
+#include "model.h"
+
+Shader* loaded_shaders;
+
+typedef struct {
+  Model* model;
+  Shader* shader;
+} Entity;
+
+Entity* newEntity(char* model_path, char* vert_shader_path, char* frag_shader_path);
+void entity_draw(Entity* entity);
+
+#endif
